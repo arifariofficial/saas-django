f221ef5f5bd8dcd6a96e0e41684acf23105c74c0
Ariful Islam <ariful6@yahoo.com>
2024-06-29 04:17:41 +0300

    src

--- src/saas/settings.py
+++ src/saas/settings.py
@@ -71,6 +71,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -159,7 +160,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_BASE_DIR = BASE_DIR / "static"
+STATICFILES_BASE_DIR = BASE_DIR.parent / "static"
 
 # source(s) for python manage.py collectstatic
 STATICFILES_DIRS = [STATICFILES_BASE_DIR]
