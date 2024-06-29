0871cc06ab1604adf2751b5f15bfbbfe5cb373ec
Ariful Islam <ariful6@yahoo.com>
2024-06-29 04:33:16 +0300

    everything to src

--- src/saas/settings.py
+++ src/saas/settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_BASE_DIR = BASE_DIR.parent / "static"
+STATICFILES_BASE_DIR = BASE_DIR / "static"
 
 # source(s) for python manage.py collectstatic
 STATICFILES_DIRS = [STATICFILES_BASE_DIR]
